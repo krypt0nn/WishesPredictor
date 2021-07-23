3725b91914093b4a46866489a968e88c00fdea52
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2021-07-24 00:33:43 +0200

    Добавьте файлы проекта.

---
+++ Main.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace GenshinWishesPredictor
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.anyChance = new System.Windows.Forms.Label();
+            this.eventChance = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(45, 123);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(133, 190);
+            this.textBox1.MaxLength = 12;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "12000";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.UpdateProbabilities);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(128, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 52);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Amount of primogems:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans SemiBold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label2.Location = new System.Drawing.Point(38, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Genshin Impact";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(262, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Wishes Predictor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(58, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(353, 26);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Your chance to get any 5* character is";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(40, 352);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(371, 26);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Your chance to get event 5* character is";
+            // 
+            // anyChance
+            // 
+            this.anyChance.AutoSize = true;
+            this.anyChance.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.anyChance.ForeColor = System.Drawing.Color.SteelBlue;
+            this.anyChance.Location = new System.Drawing.Point(407, 312);
+            this.anyChance.Name = "anyChance";
+            this.anyChance.Size = new System.Drawing.Size(18, 26);
+            this.anyChance.TabIndex = 7;
+            this.anyChance.Text = "-";
+            // 
+            // eventChance
+            // 
+            this.eventChance.AutoSize = true;
+            this.eventChance.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eventChance.ForeColor = System.Drawing.Color.SteelBlue;
+            this.eventChance.Location = new System.Drawing.Point(407, 352);
+            this.eventChance.Name = "eventChance";
+            this.eventChance.Size = new System.Drawing.Size(18, 26);
+            this.eventChance.TabIndex = 8;
+            this.eventChance.Text = "-";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(411, 190);
+            this.textBox2.MaxLength = 12;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(92, 22);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.UpdateProbabilities);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(45, 245);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(152, 30);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Soft warranty";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(406, 123);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(145, 52);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Amount of made wishes:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(323, 123);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(614, 422);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.eventChance);
+            this.Controls.Add(this.anyChance);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Wishes Predictor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label anyChance;
+        private System.Windows.Forms.Label eventChance;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace GenshinWishesPredictor
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateProbabilities(object sender, EventArgs e)
+        {
+            int wishes;
+
+            try
+            {
+                wishes = int.Parse(this.textBox2.Text) + int.Parse(this.textBox1.Text) / 160;
+            }
+
+            catch(Exception)
+            {
+                wishes = 0;
+            }
+
+            double anyChance = 0, eventChance = 0;
+
+            for (int i = 1; i <= Math.Min(wishes, 90); ++i)
+            {
+                double currAnyChance = Main.WishProbability(i);
+
+                anyChance += currAnyChance;
+                eventChance += currAnyChance / 2;
+
+                for (int j = 1; j <= Math.Min(wishes - i, 90); ++j)
+                    eventChance += Main.WishProbability(j) * currAnyChance / 2;
+            }
+
+            anyChance = Math.Max(Math.Min(anyChance, 1), 0);
+            eventChance = Math.Max(Math.Min(eventChance, 1), 0);
+
+            if (this.checkBox1.Checked)
+            {
+                this.eventChance.Text = (Math.Round(anyChance, 4) * 100).ToString() + "%";
+                this.eventChance.ForeColor = Main.GetProbabilityColor(anyChance);
+            }
+
+            else
+            {
+                this.anyChance.Text = (Math.Round(anyChance, 4) * 100).ToString() + "%";
+                this.eventChance.Text = (Math.Round(eventChance, 4) * 100).ToString() + "%";
+
+                this.anyChance.ForeColor = Main.GetProbabilityColor(anyChance);
+                this.eventChance.ForeColor = Main.GetProbabilityColor(eventChance);
+            }
+        }
+
+        public static double WishProbability(int wish)
+        {
+            return wish < 90 ? Math.Pow(0.994, wish - 1) * 0.006 : 1;
+        }
+
+        public static Color GetProbabilityColor(double probability)
+        {
+            return probability <= 0.15 ? Color.Red :
+                probability <= 0.3 ? Color.Orange :
+                probability <= 0.45 ? Color.SteelBlue :
+                Color.Green;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBox1.Checked)
+            {
+                this.label4.Visible = false;
+                this.anyChance.Visible = false;
+
+                this.label5.Top -= 40;
+                this.eventChance.Top -= 40;
+
+                this.Height -= 40;
+            }
+
+            else
+            {
+                this.label4.Visible = true;
+                this.anyChance.Visible = true;
+
+                this.label5.Top += 40;
+                this.eventChance.Top += 40;
+
+                this.Height += 40;
+            }
+
+            this.UpdateProbabilities(sender, e);
+        }
+    }
+}
